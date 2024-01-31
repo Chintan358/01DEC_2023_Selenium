f97cd3575b41cf0a341da5e51df2fc012c619011
clds <chintan.tops@gmail.com>
2024-01-31 16:43:35 +0530

    locataors

---
+++ Learnig_Selenium/src/util/Driverconnection.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driverconnection {
+	
+	public static WebDriver connect(String url)
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Chintan_work\\seleniumdata\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//Thread.sleep(2000);
+		return driver;
+	}
+}
